63c3ae9fa22224cbabe61aee1827ff33544cb0a3
Camilo Riviere <SameThingButCheaper@SameThingButCheaper.com>
2017-01-12 16:10:37 -0500

    Dir added

---
+++ python_weather_mailer/get_emails.py
@@ -0,0 +1,15 @@
+def get_emails():
+    #Reading emails from a file
+    emails = {}
+
+    try:
+        email_file = open('emails.txt', 'r')
+
+        for line in email_file:
+            (email, name) = line.split(',')
+            emails[email] = name.strip()
+
+    except FileNotFoundError as err:
+        print(err)
+
+    return emails
---
+++ python_weather_mailer/weather.py
@@ -0,0 +1,19 @@
+import requests
+def get_weather_forecast():
+
+    #Connecting to the Weather API
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=Miami&units=imperial&appid=aff57df94aca6994be2dc6dcbc0820cd'
+    weather_request = requests.get(url)
+    weather_json = weather_request.json()
+
+    #Parsing JSON
+    description = weather_json['weather'][0]['description']
+    temp_min = weather_json['main']['temp_min']
+    temp_max = weather_json['main']['temp_max']
+
+    #Creating forecast String
+    forecast = 'The forecast for Miami today is '
+    forecast += description + ' with a high of ' + str(temp_max)
+    forecast += ' and a low of ' + str(temp_min) + '.'
+
+    return forecast
